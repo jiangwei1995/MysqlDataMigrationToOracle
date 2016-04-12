5f1276507845fe42fe021a094ab31e05464b7dcf
jiangwei <jiangwei@linux.site>
2016-04-12 14:49:47 +0800

    并行导入

--- parallel.js
+++ parallel.js
@@ -8,15 +8,15 @@ var importTools = require('./importTools');
 console.time("exec-date");
 async.parallel([
  function(callback){
-    importTools.executeSrcipt("ctl/im_saleout1.ctl").then(result){
-      callback(err,result);
-    }
+    importTools.executeSrcipt("ctl/im_saleout1.ctl").then(function(result){
+      callback(null,result);
+    })
   },function(callback){
-     importTools.executeSrcipt("ctl/im_saleout.ctl").then(result){
-       callback(err,result);
-     }
+     importTools.executeSrcipt("ctl/im_saleout.ctl").then(function(result){
+       callback(null,result);
+     })
    }]
-},function(result){
+},function(err,result){
   console.log(result);
   console.timeEnd("exec-date");
 })
